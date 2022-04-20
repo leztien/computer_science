edf8a17aba9d8dedeac3435c727870530b94c3cb
I.N <48551497+leztien@users.noreply.github.com>
2022-04-20 19:40:16 +0200

    Create is_prime.py

---
+++ is_prime.py
@@ -0,0 +1,14 @@
+"""
+naive function that determines whether a number is prime
+"""
+
+def is_prime(n):
+    if n == 2:
+        return True
+    if n <= 1 or n % 2 == 0:
+        return False
+
+    for i in range(3, n, 2):  # check only odd numbers
+        if n % i == 0:
+            return False
+    return True
