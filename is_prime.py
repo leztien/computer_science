e7adac1599fb89ab0a2ebc2b88ac36a136335bfe
I.N <48551497+leztien@users.noreply.github.com>
2022-04-22 19:19:25 +0200

    function determining whether a number is prime
    naive function that determines whether a number is prime

--- is_prime.py
+++ is_prime.py
@@ -3,9 +3,10 @@ naive function that determines whether a number is prime
 """
 
 def is_prime(n):
-    if n == 2:
+    if n == 2 or n == 5:
         return True
-    if n <= 1 or n % 2 == 0:
+    if n <= 1 or n % 2 == 0 or str(n).endswith('5'):
+        print("\nFALSE")
         return False
 
     for i in range(3, n, 2):  # check only odd numbers
