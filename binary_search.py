9fe54bd67dfa7e0b5bef354af93e244143a41887
I.N <48551497+leztien@users.noreply.github.com>
2022-06-16 14:58:30 +0200

    binary search
    binary search

--- binary_search.py
+++ binary_search.py
@@ -1,4 +1,3 @@
-
 """
 binary search
 """
@@ -11,7 +10,7 @@ def make_search_space(n, max_value=None):
     
 
 
-def binary_search(seq, item):
+def iterative_binary_search(seq, item):
     while True:
         ix = len(seq) // 2
                 
@@ -24,17 +23,73 @@ def binary_search(seq, item):
             seq = seq[ix+1:]
         else:
             return False
+        
+        
+        
+def recursive_binary_search(seq, item, lo=None, hi=None):
+    lo = lo or 0
+    hi = len(seq)-1 if hi is None else hi
+    m = (lo + hi) // 2
     
+    # Base Case
+    if item == seq[m]:
+        return m
+    
+    # if "overflow" then return -1 (not found)
+    if lo > hi: return -1
+    
+    # Recursive Case
+    if item < seq[m]:
+        hi = m - 1
+    elif item > seq[m]:
+        lo = m + 1
+    # recursion
+    return recursive_binary_search(seq, item, lo, hi)
+
+        
+    
+
+def recursive_binary_search_from_geeksforgeeks(arr, item, low, high):
+    # from https://www.geeksforgeeks.org/python-program-for-binary-search/
+    # Check base case
+    if high >= low:
+ 
+        mid = (high + low) // 2
+ 
+        # If element is present at the middle itself
+        if arr[mid] == item:
+            return mid
+ 
+        # If element is smaller than mid, then it can only
+        # be present in left subarray
+        elif arr[mid] > item:
+            return recursive_binary_search_from_geeksforgeeks(arr, item, low, mid - 1)
+ 
+        # Else the element can only be present in right subarray
+        else:
+            return recursive_binary_search_from_geeksforgeeks(arr, item, mid + 1, high)
+ 
+    else:
+        # Element is not present in the array
+        return -1
+
+
 
 
 if __name__ == '__main__':
     for _ in range(1000):
-        seq = make_search_space(10)
+        print()
+        n = randint(2, 20)
+        seq = make_search_space(n)
         item = seq[randint(0, len(seq)-1)] if randint(0,1) else -1
-        ans = binary_search(seq, item=item)
-        print(item, seq, ans)
-        if ans != (item in seq):
-            raise Exception(f"item={item}\tseq={seq}")
-
-
-
+        ans = recursive_binary_search(seq, item)
+        
+        # a little bit hackish
+        print("item =", item, seq[ans] if item in seq else -1, seq, "index =", ans, seq.index(item) if item in seq else -1, item==seq[ans] if item != -1 else item == ans)
+        
+        if (item ==-1 and ans != -1):
+            raise Exception(f"item={item}\t\tseq={seq} (1)")
+        if item == ans:
+            continue
+        if (item != seq[ans]):
+            raise Exception(f"item={item}\t\tseq={seq} (2)")
