529d7c061cb44936d50c24923bac009d0ddc544c
Ilja Neitzel <48551497+leztien@users.noreply.github.com>
2024-06-18 12:42:15 +0200

    Create priority_queue.py

---
+++ priority_queue.py
@@ -0,0 +1,75 @@
+
+
+"""
+Priority Queue implemented with a binary heap
+https://www.youtube.com/watch?v=eVq8CmoC1x8&t=68s
+"""
+
+
+class Heap:
+    def __init__(self):
+        self._nodes = []
+    
+    def __len__(self):
+        return len(self._nodes)
+
+    def add(self, item):
+        # alias for convenience
+        nodes = self._nodes
+        
+        # append to the end
+        nodes.append(item)
+        
+        # bubble up
+        c = len(nodes) - 1
+        while c:
+            p = (c - (1 if c%2 else 2)) // 2
+            if nodes[c] < nodes[p]:
+                nodes[c], nodes[p] = nodes[p], nodes[c]
+                c = p
+            else:
+                break
+    
+    def pop(self):
+        # alias for convenience
+        nodes = self._nodes
+        
+        # swap
+        nodes[0], nodes[-1] = nodes[-1], nodes[0]
+        
+        # pop
+        item = nodes.pop()
+        
+        # bubble down
+        p = 0
+        while True:
+            # determine the indeces
+            l = (2*p + 1) if (2*p + 1) < len(nodes) else None
+            r = (2*p + 2) if (2*p + 2) < len(nodes) else None
+            c = r if (l and r) and (nodes[r] < nodes[l]) else l
+            
+            # swap if necessary
+            if c and nodes[c] < nodes[p]:
+                nodes[p], nodes[c] = nodes[c], nodes[p]
+                p = c
+            else: break
+        return item
+        
+
+# DEMO / TEST
+if __name__ == '__main__':
+    from random import randint, shuffle
+    
+    heap = Heap()
+    
+    for _ in range(randint(1, 100)):
+        heap.add(randint(0, 100))
+    
+    ls = [heap.pop() for _ in range(len(heap))]
+    
+    print(ls, ls == sorted(ls))
+    
+
+
+
+
